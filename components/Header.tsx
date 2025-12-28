1d9becc9d0929d0014123b449bc51f58206e5914
denizsemerci <deniz.semerci1036@gmail.com>
2025-12-28 03:43:09 +0300

    feat: Add Egeli Betty logo and update header to use logo image

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
         <Link href="/" className="flex items-center gap-3 group">
           <div className="relative w-32 h-12 md:w-40 md:h-14">
             <Image
-              src="/logo.png"
+              src="/egelibetty-logo.png"
               alt="Egeli Betty Logo"
               fill
               className="object-contain group-hover:opacity-90 transition-opacity"
